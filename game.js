0ea9bdef38c17b7e911b978a75263ade4a0521ca
Seif Sedky <Seif.alaa1231@gmail.com>
2025-07-06 22:48:47 +0300

    Implement game logic in console

--- game.js
+++ game.js
@@ -1,1 +1,53 @@
-console.log("Hello");
+let humanScore = 0;
+let computerScore = 0;
+
+
+function getComputerChoice() {
+    let n = Math.floor(Math.random() * 3);
+    if (n == 0) {
+        return 'rock';
+    }
+    else if (n == 1) {
+        return 'paper';
+    }
+    else return 'scissors';
+}
+
+function getHumanChoice() {
+    let x = prompt("Enter your choice");
+    return x;
+}
+
+
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    let win = false;
+    if (humanChoice === computerChoice) {
+        console.log(`It's a draw! You both chose ${humanChoice}.`);
+        return;
+    }
+
+
+    if (humanChoice === "scissors" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "rock" && computerChoice === "scissors") {
+        win = true;
+    }
+    if (win) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+    }
+    else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+
+    }
+}
+
+function playGame(rounds) {
+    for (let i = 0; i < rounds; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+    console.log(`The computer won ${computerScore} rounds, you won ${humanScore} rounds.`)
+}
+
+playGame(5);
