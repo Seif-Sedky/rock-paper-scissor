c2ed694cd7b36fbfb542e1d4d8a8204c2bd85ceb
Seif Sedky <Seif.alaa1231@gmail.com>
2025-07-08 07:49:03 +0300

    Add dynamic game logic using javascript

--- game.js
+++ game.js
@@ -1,6 +1,18 @@
+const buttonsContainer = document.querySelector(".buttons");
+buttonsContainer.addEventListener("click", (e) => {
+    playRound(getHumanChoice(e), getComputerChoice())
+});
+
+const roundDescriber = document.querySelector("#round-decription");
+
+const score = document.querySelector("#score");
+
+const body = document.querySelector("#game-content");
 let humanScore = 0;
 let computerScore = 0;
 
+const winner = document.createElement("p");
+body.appendChild(winner);
 
 function getComputerChoice() {
     let n = Math.floor(Math.random() * 3);
@@ -13,41 +25,48 @@ function getComputerChoice() {
     else return 'scissors';
 }
 
-function getHumanChoice() {
-    let x = prompt("Enter your choice");
-    return x;
+function getHumanChoice(e) {
+    return e.target.id;
 }
-
 
 
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
     let win = false;
     if (humanChoice === computerChoice) {
-        console.log(`It's a draw! You both chose ${humanChoice}.`);
-        return;
-    }
-
-
-    if (humanChoice === "scissors" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "rock" && computerChoice === "scissors") {
-        win = true;
-    }
-    if (win) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        roundDescriber.textContent = `It's a draw! You both chose ${humanChoice}.`;
         humanScore++;
-    }
-    else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
         computerScore++;
+    }
+
+
+    else {
+        if (humanChoice === "scissors" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "rock" && computerChoice === "scissors") {
+            win = true;
+        }
+        if (win) {
+            roundDescriber.textContent = `You win! ${humanChoice} beats ${computerChoice}.`
+            humanScore++;
+        }
+        else {
+            roundDescriber.textContent = `You lose! ${computerChoice} beats ${humanChoice}.`;
+            computerScore++;
+
+        }
+    }
+    score.textContent = `Current score: User = ${humanScore} Computer = ${computerScore}`;
+
+    if (Math.abs(humanScore - computerScore) >= 3 || humanScore + computerScore >= 5) {
+
+
+        let winnerString = (computerScore > humanScore) ? " The computer" : " The user";
+        winner.textContent = "The game has ended." + winnerString + " has won with a score of "+humanScore+" - "+computerScore;
+        humanScore = 0;
+        computerScore = 0;
+        score.textContent = `Current score: User = 0 Computer = 0`;
+
 
     }
+
 }
 
-function playGame(rounds) {
-    for (let i = 0; i < rounds; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    console.log(`The computer won ${computerScore} rounds, you won ${humanScore} rounds.`)
-}
-
-playGame(5);
